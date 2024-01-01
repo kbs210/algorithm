cd6b4c539e985b093838d6213ce3a04f6249493d
kbs_laptop <kbs@direa.co.kr>
2024-01-01 17:26:29 +0900

    baekjoon 01008

---
+++ baekjoon/01008/Problem_01008.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Problem_01008 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a/b);
+    }
+}
+
