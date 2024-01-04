57e9b5ada57e44073de267a4839b42a72198bca7
kbs210 <rlaqjatn210@naver.com>
2024-01-04 23:58:00 +0900

    baekjoon 01010 ing

--- baekjoon/01010/Problem_01010.java
+++ baekjoon/01010/Problem_01010.java
@@ -8,22 +8,39 @@ public class Problem_01010 {
         int a;
         a = sc.nextInt();
 
-        int[] list = new int[a];
+        long[] list = new long[a];
 
         for(int i=0; i<a; i++) {
             int b, c;
             b = sc.nextInt();
             c = sc.nextInt();
 
-            int s = c-b+1;
-            for(int x=s; x>0; --x) {
-                for(int y=x; y>0; --y){
-                    list[i] = list[i]+y;
-                }
+            long N = b;
+            long M = c;
+            long P = c-b;
+
+            long numerator = 1;
+            for(int x=0; x<c; x++) {
+                numerator = numerator*M;
+                M--;
             }
+
+            long denominatorFirst = 1;
+            for(int x=0; x<b; x++) {
+                denominatorFirst = denominatorFirst * N;
+                N--;
+            }
+
+            long denominatorSecond = 1;
+            for(int x=0; x<(c-b); x++) {
+                denominatorSecond = denominatorSecond * P;
+                P--;
+            }
+
+            list[i] = numerator/(denominatorFirst*denominatorSecond);
         }
 
-        for (int value: list) {
+        for (long value: list) {
             System.out.println(value);
         }
 
