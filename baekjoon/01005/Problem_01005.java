3657f2ea266ae99d34186a303318cb328adfc1ae
kbs <kbs@direa.co.kr>
2024-01-16 20:56:40 +0900

    baekjoon 01005 ing 시간초과

--- baekjoon/01005/Problem_01005.java
+++ baekjoon/01005/Problem_01005.java
@@ -1,10 +1,12 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Problem_01005 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // ACM Craft
 
@@ -19,28 +21,34 @@ public class Problem_01005 {
         // 1 <= K <= 100000
         // 1 <= X, Y, W <= N
         // 0 <= Di <= 100000 정수
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
         int[] resultArray = new int[T];
 
-        int t=0;
         for (int i = 0; i < T; i++) {
-            int N = scanner.nextInt();
-            int K = scanner.nextInt();
+            String line1 = br.readLine();
+            StringTokenizer st1 = new StringTokenizer(line1, " ");
+
+            int N = Integer.parseInt(st1.nextToken());
+            int K = Integer.parseInt(st1.nextToken());
 
             int[] dArray = new int[N];
             int[][] xyArray = new int[K][2];
 
+            String line2 = br.readLine();
+            StringTokenizer st2 = new StringTokenizer(line2, " ");
             for (int j = 0; j < N; j++) {
-                dArray[j] = scanner.nextInt();
+                dArray[j] = Integer.parseInt(st2.nextToken());
             }
 
             for (int j = 0; j < K; j++) {
-                xyArray[j][0] = scanner.nextInt();
-                xyArray[j][1] = scanner.nextInt();
+                String line3 = br.readLine();
+                StringTokenizer st3 = new StringTokenizer(line3, " ");
+                xyArray[j][0] = Integer.parseInt(st3.nextToken());
+                xyArray[j][1] = Integer.parseInt(st3.nextToken());
             }
 
-            int W = scanner.nextInt();
+            int W = Integer.parseInt(br.readLine());
 
             // 로직 시작
 
@@ -58,15 +66,16 @@ public class Problem_01005 {
                 }
             }
 
-            boolean check=false;
+            boolean check;
             do {
+                check=false;
                 for (int a = 0; a < N; a++) {
                     if (base[a] == 0) {
                         ArrayList<Integer> list = new ArrayList<>();
                         boolean flag = false;
                         for (int b = 0; b < K; b++) {
                             if (xyArray[b][1] == a + 1) {
-                                if(base[xyArray[b][0] - 1] == -1) {
+                                if (base[xyArray[b][0] - 1] == -1) {
                                     list.add(xyArray[b][0]);
                                 } else {
                                     flag = true;
@@ -90,25 +99,26 @@ public class Problem_01005 {
                     }
                 }
 
-                for (int a = 0; a < N; a++) {
-                    if (base[a] == 0) {
-                        check=true;
+                for (int d = 0; d < N; d++) {
+                    if (base[d] == 0) {
+                        check = true;
                         break;
                     }
                 }
             } while (check);
 
 
-            resultArray[t] = buildTime[W-1];
-            t++;
-
+            resultArray[i] = buildTime[W - 1];
             // 로직 종료
         }
 
-        for (int value: resultArray) {
-            System.out.println(value);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int a = 0; a < T; a++) {
+            bw.write(resultArray[a] + "\n");
         }
-        scanner.close();
+        bw.flush();
+        bw.close();
+        br.close();
 
     }
 
