e62a7d65b70d0cb36a7a37a208908ad26af32014
kbs210 <rlaqjatn210@naver.com>
2024-01-27 13:33:10 +0900

    baekjoon 01006 ing

---
+++ baekjoon/01006/Problem_01006.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Problem_01006 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            ArrayList<Integer> innerPosition = new ArrayList<>();
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                innerPosition.add(Integer.parseInt(st.nextToken()));
+            }
+            ArrayList<Integer> outterPosition = new ArrayList<>();
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                outterPosition.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        br.close();
+        bw.close();
+    }
+
+}
